5589f1808316ba676d227490f8eca48714cdfa0d
khalouiyesser <111162300+khalouiyesser@users.noreply.github.com>
2024-12-11 09:37:01 +0100

    9bal validation

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.light(), // Thème clair par défaut
       darkTheme: ThemeData.dark(), // Thème sombre par défaut
       themeMode: themeProvider.themeMode, // Thème géré dynamiquement
-      home: LoginScreen(), // Définit HomeScreen comme écran d'accueil
+      home: WelcomeScreen(), // Définit HomeScreen comme écran d'accueil
     );
   }
 }
